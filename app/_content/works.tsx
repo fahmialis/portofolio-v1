dc416c379865b4d946b69b0f503cb60218db8e25
fahmialis <fsinergia@yahoo.com>
2025-12-02 15:31:52 +0700

    add works section

--- app/_content/works.tsx
+++ app/_content/works.tsx
@@ -1,47 +1,60 @@
-import { InfoRow } from "../_sections/infoSection";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
 import Image from "next/image";
+import { InfoRow } from "../_sections/infoSection";
 
 const worksContent: InfoRow[] = [
   {
     title: "Superapp Dashboard V3",
     content: (
       <section>
-        <div className='flex justify-between mb-4'>
-          <div>
-            <div>Description</div>
-            PT. Bank Rakyat Indonesia loyalty program dashboard used to monitor
-            and analyze the performance of the loyalty program.
+        <div className='flex justify-between mb-8'>
+          <div className='w-2/3'>
+            <div className='text-[16px] font-semibold'>Description</div>
+            <div>
+              Internal dashboard for Superapp dashboard used to monitor and
+              analyze the performance of Superapp procurement and transaction.
+            </div>
           </div>
 
-          <div>
-            <div>Tech used</div>
-            <div>Techstack: Reactjs, Typescript, Tailwindcss</div>
+          <div className='w-1/3'>
+            <div className='text-[16px] font-semibold'>Tech used</div>
+            <div>
+              {" "}
+              <strong>Frontend:</strong> Reactjs, Nextjs, Jotai, Zod, Tanstack
+              React Query
+              <br />
+              <strong>Testing:</strong> Jest
+              <br />
+              <strong>Architecture:</strong> Micro Frontend
+            </div>
           </div>
         </div>
 
-        <Carousel className='w-full'>
+        <Carousel className='w-full mb-8'>
           <CarouselContent className='-ml-1'>
-            {["/super_v3_1.png", "/super_v3_2.png", "/super_v3_3.png",  "/super_v3_4.png"].map(
-              (path, index) => (
-                <CarouselItem
-                  key={index}
-                  className='pl-1 md:basis-1/2 lg:basis-1/3'
-                >
-                  <Image
-                    className='h-64 w-full object-cover object-center'
-                    src={path}
-                    alt='alt'
-                    width={400}
-                    height={720}
-                  />
-                </CarouselItem>
-              )
-            )}
+            {[
+              "/super_v3_1.png",
+              "/super_v3_2.png",
+              "/super_v3_3.png",
+              "/super_v3_4.png",
+            ].map((path, index) => (
+              <CarouselItem
+                key={index}
+                className='pl-1 md:basis-1/2 lg:basis-1/3'
+              >
+                <Image
+                  className='h-64 w-full object-cover object-center'
+                  src={path}
+                  alt='alt'
+                  width={400}
+                  height={720}
+                />
+              </CarouselItem>
+            ))}
           </CarouselContent>
         </Carousel>
       </section>
@@ -49,77 +62,96 @@ const worksContent: InfoRow[] = [
   },
   {
     title: "Superapp Dashboard V4",
-    content:       <section>
-        <div className='flex justify-between mb-4'>
-          <div>
-            <div>Description</div>
-            PT. Bank Rakyat Indonesia loyalty program dashboard used to monitor
-            and analyze the performance of the loyalty program.
+    content: (
+      <section>
+        <div className='flex justify-between mb-8'>
+          <div className='w-2/3'>
+            <div className='text-[16px] font-semibold'>Description</div>
+            <div>
+              Internal dashboard for Superapp dashboard used to monitor and
+              analyze the performance of Superapp procurement and transaction.
+            </div>
           </div>
 
-          <div>
-            <div>Tech used</div>
-            <div>Techstack: Reactjs, Typescript, Tailwindcss</div>
+          <div className='w-1/3'>
+            <div className='text-[16px] font-semibold'>Tech used</div>
+            <div>
+              <strong>Frontend: </strong>Reactjs, Nextjs, Jotai, Zod, Tanstack
+              React Query, React Hook Form, AntDesign
+              <br />
+              <strong>Testing:</strong>Vitest, Jest
+            </div>
           </div>
         </div>
 
-        <Carousel className='w-full'>
+        <Carousel className='w-full mb-8'>
           <CarouselContent className='-ml-1'>
-            {["/super_v4_1.png", "/super_v4_2.png", "/super_v4_3.png", "/super_v4_4.png", "/super_v4_5.png"].map(
-              (path, index) => (
-                <CarouselItem
-                  key={index}
-                  className='pl-1 md:basis-1/2 lg:basis-1/3'
-                >
-                  <Image
-                    className='h-64 w-full object-cover object-center'
-                    src={path}
-                    alt='alt'
-                    width={400}
-                    height={720}
-                  />
-                </CarouselItem>
-              )
-            )}
+            {[
+              "/super_v4_1.png",
+              "/super_v4_2.png",
+              "/super_v4_3.png",
+              "/super_v4_4.png",
+              "/super_v4_5.png",
+            ].map((path, index) => (
+              <CarouselItem
+                key={index}
+                className='pl-1 md:basis-1/2 lg:basis-1/3'
+              >
+                <Image
+                  className='h-64 w-full object-cover object-center'
+                  src={path}
+                  alt='alt'
+                  width={400}
+                  height={720}
+                />
+              </CarouselItem>
+            ))}
           </CarouselContent>
         </Carousel>
-      </section>,
+      </section>
+    ),
   },
   {
     title: "Superapp Storybook Component Library",
     content: (
       <section>
-        <div className='flex justify-between mb-4'>
-          <div>
-            <div>Description</div>
-            PT. Bank Rakyat Indonesia loyalty program dashboard used to monitor
-            and analyze the performance of the loyalty program.
+        <div className='flex justify-between mb-8'>
+          <div className='w-2/3'>
+            <div className='text-[16px] font-semibold'>Description</div>
+            <div>Component library for Superapp dashboard.</div>
           </div>
 
-          <div>
-            <div>Tech used</div>
-            <div>Techstack: Reactjs, Typescript, Tailwindcss</div>
+          <div className='w-1/3'>
+            <div className='text-[16px] font-semibold'>Tech used</div>
+            <div>
+              <strong>Frontend:</strong> NextJS, Typescript, Tailwindcss,
+              Storybook <br />
+            </div>
           </div>
         </div>
 
-        <Carousel className='w-full'>
+        <Carousel className='w-full mb-8'>
           <CarouselContent className='-ml-1'>
-            {["/storybook6.png", "/storybook7.png", "/storybook8.png", "/storybook4.png", "/storybook5.png"].map(
-              (path, index) => (
-                <CarouselItem
-                  key={index}
-                  className='pl-1 md:basis-1/2 lg:basis-1/3'
-                >
-                  <Image
-                    className='h-64 w-full object-contain object-center'
-                    src={path}
-                    alt='alt'
-                    width={400}
-                    height={720}
-                  />
-                </CarouselItem>
-              )
-            )}
+            {[
+              "/storybook6.png",
+              "/storybook7.png",
+              "/storybook8.png",
+              "/storybook4.png",
+              "/storybook5.png",
+            ].map((path, index) => (
+              <CarouselItem
+                key={index}
+                className='pl-1 md:basis-1/2 lg:basis-1/3'
+              >
+                <Image
+                  className='h-64 w-full object-contain object-center'
+                  src={path}
+                  alt='alt'
+                  width={400}
+                  height={720}
+                />
+              </CarouselItem>
+            ))}
           </CarouselContent>
         </Carousel>
       </section>
@@ -128,38 +160,48 @@ const worksContent: InfoRow[] = [
   {
     title: "Procash Dashboard",
     content: (
-    <section>
-        <div className='flex justify-between mb-4'>
-          <div>
-            <div>Description</div>
-            PT. Bank Rakyat Indonesia loyalty program dashboard used to monitor
-            and analyze the performance of the loyalty program.
+      <section>
+        <div className='flex justify-between mb-8'>
+          <div className='w-2/3'>
+            <div className='text-[16px] font-semibold'>Description</div>
+            <div>
+              PT. Bank Rakyat Indonesia loyalty program dashboard used to
+              monitor and analyze the performance of the loyalty program.
+            </div>
           </div>
 
-          <div>
-            <div>Tech used</div>
-            <div>Techstack: Reactjs, Typescript, Tailwindcss</div>
+          <div className='w-1/3'>
+            <div className='text-[16px] font-semibold'>Tech used</div>
+            <div>
+              <strong>Frontend:</strong> Laravel (Blade) <br />
+              <strong>Backend:</strong> Golang, grpc, sqlc <br />
+              <strong>Database:</strong> MySQL, PostgreSQL
+            </div>
           </div>
         </div>
 
-        <Carousel className='w-full'>
+        <Carousel className='w-full mb-8'>
           <CarouselContent className='-ml-1'>
-            {["/PROCASH_1.png", "/PROCASH_2.png", "/PROCASH_3.png", "/PROCASH_4.png", "/PROCASH_5.png"].map(
-              (path, index) => (
-                <CarouselItem
-                  key={index}
-                  className='pl-1 md:basis-1/2 lg:basis-1/3'
-                >
-                  <Image
-                    className='h-64 w-full object-contain object-center'
-                    src={path}
-                    alt='alt'
-                    width={400}
-                    height={720}
-                  />
-                </CarouselItem>
-              )
-            )}
+            {[
+              "/PROCASH_1.png",
+              "/PROCASH_2.png",
+              "/PROCASH_3.png",
+              "/PROCASH_4.png",
+              "/PROCASH_5.png",
+            ].map((path, index) => (
+              <CarouselItem
+                key={index}
+                className='pl-1 md:basis-1/2 lg:basis-1/3'
+              >
+                <Image
+                  className='h-64 w-full object-contain object-center'
+                  src={path}
+                  alt='alt'
+                  width={400}
+                  height={720}
+                />
+              </CarouselItem>
+            ))}
           </CarouselContent>
         </Carousel>
       </section>
