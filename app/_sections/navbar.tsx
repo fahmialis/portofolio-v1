2593122d01134b1deeb06015e6bda2a228dcc283
fahmialis <fsinergia@yahoo.com>
2025-12-03 14:45:22 +0700

    add mouse and theme toggler

--- app/_sections/navbar.tsx
+++ app/_sections/navbar.tsx
@@ -1,7 +1,11 @@
+import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
+
 export default function Navbar() {
   return (
     <section className='flex justify-between items-center'>
       <div className='text-[16px]'>Fahmi Sinergia</div>
+
+      <AnimatedThemeToggler />
     </section>
   );
 }
